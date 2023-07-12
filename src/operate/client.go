d86788754cfc957ca8ccfb4d7610f93c50a51cef
grchar <806762029@qq.com>
2023-07-12 18:10:22 +0800

    123

--- src/operate/client.go
+++ src/operate/client.go
@@ -1,13 +1,13 @@
 package operate
 
 import (
-	"bytes"
 	"changeme/src/logger"
 	"encoding/json"
 	hook "github.com/robotn/gohook"
 	"golang.org/x/net/ipv6"
 	"net"
 	"sync"
+	"time"
 )
 
 type operateStep struct {
@@ -18,13 +18,14 @@ type operateStep struct {
 type operateControl struct {
 	operateChan     chan operateStep
 	sendOperateChan chan operateStep
-	conn            net.Conn
+	conn            net.UDPConn
+	tcpConn         net.Conn
 }
 
 var operateController = operateControl{
 	operateChan:     make(chan operateStep, 10000),
 	sendOperateChan: make(chan operateStep, 10000),
-	conn:            nil,
+	conn:            net.UDPConn{},
 }
 
 func init() {
@@ -34,35 +35,56 @@ func init() {
 }
 
 func (control *operateControl) connectServer(wg *sync.WaitGroup) {
-	conn, err := net.Dial("tcp", "localhost:8001")
+	//tcpConn, err := net.Dial("tcp", "127.0.0.1:8002")
+	//if err != nil {
+	//	logger.GetLogger().Error(err)
+	//	return
+	//}
+	//control.tcpConn = tcpConn
+	//_, err = control.tcpConn.Write([]byte("test"))
+	//if err != nil {
+	//	logger.GetLogger().Info()
+	//	return
+	//}
+	//124.70.94.103
+	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:8001")
 	if err != nil {
-		logger.GetLogger().Error(err)
+		logger.GetLogger().Error("服务连接失败:", err)
 		return
 	}
-	control.conn = conn
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		logger.GetLogger().Error("服务连接失败:", err)
+		return
+	}
+	control.conn = *conn
+	control.conn.SetReadDeadline(time.Now().Add(1 * time.Second))
 	control.conn.Write([]byte("test"))
 	wg.Done()
 
 	go func() {
+
 		for {
-			buffer := bytes.Buffer{}
-			n, err := buffer.ReadFrom(control.conn)
+			buf := make([]byte, 1024)
+			n, _, err := control.conn.ReadFromUDP(buf)
+
 			if err != nil {
 				logger.GetLogger().Error(err)
 				return
 			}
-			result := buffer.Bytes()
-			logger.GetLogger().Info(string(buffer.Bytes()[:n]))
+			logger.GetLogger().Info(string(buf[:n]))
 			data := operateStep{}
-			err = json.Unmarshal(result, &data)
+			err = json.Unmarshal(buf[:n], &data)
 			if err != nil {
 				logger.GetLogger().Error(err)
 			}
 
 			control.operateChan <- data
 		}
+		//logger.GetLogger().Info(string(result))
+
 	}()
-	control.sendDataToRemoteClient(wg)
+	go control.sendDataToRemoteClient(wg)
 }
 
 func (control *operateControl) sendDataToRemoteClient(wg *sync.WaitGroup) {
@@ -71,9 +93,9 @@ func (control *operateControl) sendDataToRemoteClient(wg *sync.WaitGroup) {
 		select {
 		case step := <-control.sendOperateChan:
 			selfStep := step
-			logger.GetLogger().Info(step.ClientKey, step.Event.String())
+			//logger.GetLogger().Info(step.ClientKey, step.Event.String())
 			data, err := json.Marshal(&selfStep)
-			logger.GetLogger().Info(string(data))
+			//logger.GetLogger().Info(string(data))
 			localStep := operateStep{}
 			json.Unmarshal(data, &localStep)
 
@@ -82,6 +104,7 @@ func (control *operateControl) sendDataToRemoteClient(wg *sync.WaitGroup) {
 				continue
 			}
 			//data = append(data, byte(0xff))
+			logger.GetLogger().Info(control.conn.LocalAddr().String(), len(data))
 			_, err = control.conn.Write(data)
 			if err != nil {
 				logger.GetLogger().Error(err)
